72ef4093d6c022602dac75406eb1b61ba12057a4
Alex <alex.woods89@hotmail.co.uk>
2022-02-08 14:04:44 +0000

    build basic CRUD api

--- pokemon-api/pokemon-api/src/main/java/com/example/pokemonapi/controllers/PokeController.java
+++ pokemon-api/pokemon-api/src/main/java/com/example/pokemonapi/controllers/PokeController.java
@@ -1,13 +1,32 @@
 package com.example.pokemonapi.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.pokemonapi.models.Pokemon;
+import com.example.pokemonapi.repositories.PokemonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "/pokemon")
 public class PokeController {
 
+    @Autowired
+    PokemonRepository pokemonRepository;
+
     @GetMapping
-    public String getPokemon(){
-        return "Gotta Catch Em All";
+    public List<Pokemon> getAllPokemon(){
+        return  pokemonRepository.findAll();
+    }
+
+    @PostMapping
+    public void savePokemon(@RequestBody Pokemon pokemon){
+        pokemonRepository.save(pokemon);
+    }
+
+    @DeleteMapping
+    public void deleteAllPokemon(){
+        pokemonRepository.deleteAll();
     }
 }
+
---
+++ pokemon-api/pokemon-api/src/main/java/com/example/pokemonapi/models/Pokemon.java
@@ -0,0 +1,52 @@
+package com.example.pokemonapi.models;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="pokemon")
+public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "url")
+    private String url;
+
+    public Pokemon( String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public Pokemon(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
